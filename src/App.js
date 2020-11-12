780dd691f79ff728a322bcdd1e2342b3de9d4758
Daffa <daffamaulana30@gmail.com>
2020-11-12 22:29:31 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React from "react";
-import Pagination from "pagination-react-hooks";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  const posts = [
-    { id: "1", name: "Berat 1" },
-    { id: "2", name: "Berat 2" },
-    { id: "3", name: "Berat 3" },
-    { id: "4", name: "Berat 4" },
-    { id: "5", name: "Berat 5" },
-    { id: "6", name: "Berat 6" },
-  ];
-
-  const show = (value) => (
-    <li key={value.id} className="card">
-      <span>{value.name}</span>
-    </li>
-  );
-  console.log(1)
   return (
-    <Pagination
-      data={posts}
-      Show={show}
-      displayNumber="3"
-      previousText="Previous"
-      nextText="Next"
-    />
+    <div className="App">
+      <h1>My First Heading</h1>
+      <p>My first paragraph.</p>
+    </div>
   );
 }
 
